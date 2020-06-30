2b48df9480df16307fb8b307ee6818efe99e083e
khalid-khg <67597228+khalid-khg@users.noreply.github.com>
2020-06-30 13:54:47 -0700

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,7 @@
+# this is a hello world application
+print('hello world! \n')
+print("Hello world 2")
+# multiline
+print('''this is a very line hello world
+today id a very sunny''')
+print('khalid')
